41162af1de2d81501c64c96f4885320de5aa3692
Felipe Souza <felipesouzadsgn@gmail.com>
2022-09-16 17:59:50 -0300

    Adding components and styling in App component

--- App.tsx
+++ App.tsx
@@ -1,9 +1,35 @@
-import { Background } from './src/components/Background';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from "@expo-google-fonts/inter"
+
+import { StatusBar } from "react-native"
+
+import { Background } from './src/components/Background'
+import { Loading } from "./src/components/Loading";
+
+import { Home } from "./src/screens/Home";
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Inter_600SemiBold,
+    Inter_400Regular,
+    Inter_700Bold,
+    Inter_900Black
+  })
+
   return (
     <Background>
-
+      < StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Home /> : <Loading />}
     </Background>
   );
 }
